b8bda92ac37910e2380ceb270eb6527c16ebfe44
Favour Ori <orifavour2000@gmail.com>
2019-09-23 10:23:14 +0200

    minor

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const app = require('express')()
 
 
-//routers
+//Importing routes
 const indexRoute = require('./routes/index')
+
+
+//using routes
 app.use('/api', indexRoute)
 
+
+
+//Spin up dev Server
 let port = process.env.port || 3000
 app.listen(port, () => {
     console.log("App is running on " + port)
